480a7bf995f710b2dab7869fba8088041d689a06
bluMyst <weirdusername@gmail.com>
2015-11-03 17:04:04 -0600

    More misc fixes. Make sure to check first!
    I was slightly drunk this time so I may have slightly broken some things, but it'll just be minor syntax stuff like calling a method as method_name() instead of self.method_name()

--- quoteit.py
+++ quoteit.py
@@ -31,7 +31,7 @@ class Comments:
                 # QuoteIt! '[quote]' - u/[username]
                 # etc
 
-                quoteit! \s+
+                quoteit! \s*
                 ( ["'].*["'] )*
                 [\s-]*
 
@@ -71,7 +71,7 @@ class Comments:
             ID = comment.id
             # quote will be true when we match a properly formatted keyword call
             # the db lookup is to make sure we don't reply to the same post
-            if quote and not self.db.lookup_ID(ID):
+            if quote and not self.db.lookup_id(ID):
                 results.append((comment, quote, user))
 
         return results
@@ -79,12 +79,12 @@ class Comments:
     def parse_for_keywords(self, comment):
         # search for keyword string
         match = re.findall(self.regex, str(comment))
+
         try:
             # match will be None if we don't
             # find the keyword string
             quote = match[0][0]
             user = match[0][1]
-
         except IndexError:
             quote = False
             user = False
@@ -101,62 +101,69 @@ class Respond:
 
     REGEX = re.compile(
         """
-            Quoting \s+
-            ( [/u/]* [\w_' -]* ): \s+
+            Quoting
+            \s*
+            ( [/u/]* [\w_' -]* ):
+            \s*
             (".*")
         """,
-       flags = re.IGNORECASE  | re.UNICODE | re.VERBOSE,
+        flags = re.IGNORECASE  | re.UNICODE | re.VERBOSE,
     )
 
     FOOTER = (
         # TODO: Experiment with ^(string of text) instead of ^string ^of ^text.
-        "\n\n___\n\n"
-         "^If ^this ^post ^receives ^enough ^upvotes, ^it ^will "
-         "^be ^submitted ^to ^/r/Quotes! "
-         "^| [^Code](https://github.com/cameron-gagnon/quoteitbot) "
+        "\n\n___\n\n" +
+         "^If ^this ^post ^receives ^enough ^upvotes, ^it ^will " +
+         "^be ^submitted ^to ^/r/Quotes! " +
+         "^| [^Code](https://github.com/cameron-gagnon/quoteitbot) " +
          "^| [^About ^me]({link})"
          #"^| ^Syntax: ^'QuoteIt! ^\"Insert ^quote ^here\" ^/u/username' "
     )
 
     def __init__(self, r):
-        self.r = r
+        self.r  = r
         self.db = Database()
 
     def reply(self, results):
         for comment, quote, user in results:
             try:
                 self.reply_quote(comment, quote, user)
-                pass
             except praw.errors.InvalidComment:
                 log.warning("Comment was deleted")
-                pass
 
             self.db.insert(comment.id)
 
     def reply_quote(self, comment, quote, user):
         comment_author = str(comment.author)
-        reply_string = self.REPLY_TEXT.format(user = user,
-                                              quote = quote) +\
-                       self.FOOTER.format(link = self.NON_SPAM_LINK)
 
-        log.debug("Replying to " + comment_author +
-                  " with quote " + quote + " from user " + user)
+        reply_string  = self.REPLY_TEXT.format(user=user, quote=quote)
+        reply_string += self.FOOTER.format(link=self.NON_SPAM_LINK)
+
+        log.debug(
+            "Replying to " + comment_author
+            + " with quote " + quote
+            + " from user " + user
+        )
 
         try:
             comment.reply(reply_string)
+
             # alert user begin queried of query
             log.debug("Reply sucessful!")
-
         except praw.errors.RateLimitExceeded as error:
-            log.debug("Rate limit exceeded, must sleep for "
-                      "{} mins".format(float(error.sleep_time / 60)))
+            log.debug(
+                "Rate limit exceeded. Must sleep for "
+                + (error.sleep_time / 60) + " mins."
+            )
+
             time.sleep(error.sleep_time)
+
             # try to reply to the comment again
             comment.reply(reply_string)
-            log.debug("Reply sucessful!")
 
+            log.debug("Reply sucessful!")
         except praw.errors.HTTPException as error:
-            log.debug("HTTPError when replying. Sleeping for 10 seconds")
+            log.debug("HTTPError when replying. Sleeping for 10 seconds.")
             log.debug(error)
             time.sleep(30)
 
@@ -164,6 +171,7 @@ class Respond:
         #log.debug("Checking votes")
         # get our quoteitbot
         r = self.r.get_redditor("QuoteItBot")
+
         # return all comments to see their scores
         # if any are > 10, we will post it to quotes
         # a time interval for last comment can be set
@@ -172,12 +180,12 @@ class Respond:
         for comment in comments:
             # comment ID here is the comment ID of QuoteItBot's comment with the
             # quote in it
-            if (comment.score > self.UPVOTE_THRESHOLD) and not\
-                self.db.lookup_post(comment.id):
+            if (comment.score > self.UPVOTE_THRESHOLD and not
+                    self.db.lookup_post(comment.id)):
                 self.post_to_quotes(comment)
 
     def post_to_quotes(self, comment):
-        text = comment.body
+        text          = comment.body
         about_me_link = self.NON_SPAM_LINK
 
         # pull out the username and quote from our old post
@@ -187,47 +195,51 @@ class Respond:
             log.debug("Match is: ")
             log.debug(str(match))
             username = match[0][0]
-            quote = match [0][1]
-
+            quote    = match[0][1]
         except IndexError:
-            log.debug("Not an actual quote comment, index error returned")
-            self.db.insert_post(comment.id) 
+            log.debug("Not an actual quote comment; index error returned.")
+            self.db.insert_post(comment.id)
             return False
 
-        parent_author = self.r.get_info(thing_id = comment.parent_id)
+        parent_author = self.r.get_info(thing_id=comment.parent_id)
+
         # check for nsfw sub that the comment was posted on
         # apply shortened url to send the post directly to spam
         f = Filter(self.r)
-        if f.filter_nsfw(self.r, comment) and not\
-           f.blacklisted_user(username) and not\
-           f.blacklisted_user(parent_author.author):
-            about_me_link = self.SPAM_LINK
+        if (f.filter_nsfw(self.r, comment) and not
+               f.blacklisted_user(username) and not
+               f.blacklisted_user(parent_author.author)):
+           about_me_link = self.SPAM_LINK
 
+        title = "[QuoteItBot] {quote} - {username}".format(
+            quote=quote, username=username,
+        )
 
-        title = "[QuoteItBot] " + quote + " - " + username
-        # gets lots of submission data and pieces it together 
-        # so we can have the premalink to the top level comment
-                        # base url                 what subreddit we in?
-        formatted_url = ("https://reddit.com/r/" + str(comment.subreddit) +
-                        "/comments/" + comment.link_id[3:] + "/" +
-                        comment.link_title + "/" + comment.parent_id[3:])
-                        # title of submission       id of the parent comment of the
-                        #                           quoteitbot one
+        # gets lots of submission data and pieces it together
+        # so we can have the permalink to the top-level comment
+        formatted_url  = "https://reddit.com/r/" + str(comment.subreddit)
+        formatted_url += "/comments/" + comment.link_id[3:] + "/"
+        formatted_url += comment.link_title + '/' + comment.parent_id[3:]
 
-        body = ("[Original quote source](" + formatted_url + ")." +
-                self.FOOTER.format(about_me_link))
+        body  = "[Original quote source](" + formatted_url + ")."
+        body += self.FOOTER.format(about_me_link)
 
         try:
             log.debug("Submitting quote: " + title)
-            self.r.submit("Quotes", title, text = body)
+            self.r.submit("Quotes", title, text=body)
 
             log.debug("Submission sucessful!")
             self.db.insert_post(comment.id)
-
         except praw.errors.RateLimitExceeded as error:
-            log.debug("Rate limit exceeded for posting, must sleep for "
-                      "{} mins".format(float(error.sleep_time / 60)))
+            # TODO: This code gets repeated. Make a function or something.
+            #       Don't repeat yourself.
+            log.debug(
+                "Rate limit exceeded for posting; must sleep for "
+                + (error.sleep_time / 60) + "mins"
+            )
+
             time.sleep(error.sleep_time)
+
             # try to reply to the comment again
             self.r.submit("Quotes", title, body)
             log.debug("Submission sucessful!")
@@ -235,25 +247,24 @@ class Respond:
 
 class Filter:
     def __init__(self, r):
-        self.r = r
+        self.r  = r
         self.db = Database()
 
     def filter_nsfw(self, r, comment):
-        subreddit_ID = comment.subreddit_id
-        # returns true if the subreddit is over18, AKA NSFW.
-        return r.get_info(thing_id(subreddit_ID).over18)
+        """ returns true if the subreddit is over18, AKA NSFW. """
+        return r.get_info(thing_id(comment.subreddit_id).over18)
 
     def blacklisted_user(self, user):
-        # returns true if the user is in the database, AKA 'blacklisted'
+        """ returns true if the user is in the database, AKA 'blacklisted' """
         return self.db.lookup_user(user)
 
     def check_mail(self):
         #log.debug("Checking mail")
-        messages = self.r.get_unread(unset_has_mail = True, update_user = True)
+        messages = self.r.get_unread(unset_has_mail=True, update_user=True)
 
         for msg in messages:
-            if str(msg.author).lower() == "camerongagnon" and\
-               msg.subject.lower() == "blacklist":
+            if (str(msg.author).lower() == "camerongagnon" and
+                    msg.subject.lower() == "blacklist"):
                 log.debug("Received message from camerongagnon")
 
                 # splits the message on spaces and '\n'
@@ -276,44 +287,42 @@ class Database:
         self.sql = sqlite3.connect('IDs.db')
         self.cur = self.sql.cursor()
 
-        self.cur.execute('CREATE TABLE IF NOT EXISTS\
-                          quotes(ID TEXT, ID_post TEXT, users TEXT)')
+        self.cur.execute(
+            'CREATE TABLE IF NOT EXISTS quotes(ID TEXT, ID_post TEXT, users TEXT)'
+        )
+
         self.sql.commit()
 
-    def insert(self, ID):
+    def _insert_thing(self, thing, arg):
+        self.cur.execute('INSERT INTO quotes (' + thing + ') VALUES (?)', [arg])
+        self.sql.commit()
+
+    def insert(self, id_):
         """ Add ID to comment database so we know we already replied to it """
-        self.cur.execute('INSERT INTO quotes (ID) VALUES (?)', [ID])
-        self.sql.commit()
+        self._insert_thing('ID', id_)
+        log.debug("Inserted " + str(id_) + " of comment into database!")
 
-        log.debug("Inserted " + str(ID) + " of comment into database!")
-
-    def insert_post(self, ID):
-        self.cur.execute('INSERT INTO quotes (ID_post) VALUES (?)', [ID])
-        self.sql.commit()
-
-        log.debug("Inserted " + str(ID) + " of post into database.")
+    def insert_post(self, id_):
+        self._insert_thing('ID_post', id_)
+        log.debug("Inserted " + str(id_) + " of post into database.")
 
     def insert_user(self, user):
-        self.cur.execute('INSERT INTO quotes (users) VALUES (?)', [user])
-        self.sql.commit()
-
+        self._insert_thing('users', user)
         log.debug("Inserted " + str(user) + " into blacklisted users")
 
-    def lookup_ID(self, ID):
-        """ See if the ID has already been added to the database.  """
-        self.cur.execute('SELECT * FROM quotes WHERE ID=?', [ID])
-        result = self.cur.fetchone()
-        return result
+    def _lookup_thing(self, thing, arg):
+        self.cur.execute('SELECT * FROM quotes WHERE ' + thing + '=?', [arg])
+        return self.cur.fetchone()
 
-    def lookup_post(self, ID):
-        self.cur.execute('SELECT * FROM quotes WHERE Id_post=?', [ID])
-        result = self.cur.fetchone()
-        return result
+    def lookup_id(self, id_):
+        """ See if the ID has already been added to the database.  """
+        return self._lookup_thing('ID', id_)
+
+    def lookup_post(self, id_):
+        return self._lookup_thing('Id_post', [id_])
 
     def lookup_user(self, user):
-        self.cur.execute('SELECT * FROM quotes WHERE users=?', [user])
-        result = self.cur.fetchone()
-        return result
+        return self._lookup_thing('users', user)
 
 #######################################################
 # Makes stdout and stderr print to the logging module #
